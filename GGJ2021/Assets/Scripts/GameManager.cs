ad0154c53106eace3716444d54eedf995379f86f
BensonCoolfi <78238939+BensonCoolfi@users.noreply.github.com>
2021-01-30 16:36:34 +0200

    new stuff 16:36

---
+++ GGJ2021/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject demonContainer, hudContainer;
+    public TextMeshProUGUI ddemonCounter, timeCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
